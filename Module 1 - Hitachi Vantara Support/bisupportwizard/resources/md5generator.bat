b29424b9f93893132d4eb59736f79d7a5509ab7d
James O'Reilly <jp.oreilly@hotmail.com>
2019-01-09 19:39:07 +0000

    updates support utility

--- Module 1 - Hitachi Vantara Support/bisupportwizard/resources/md5generator.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-set CLASSPATH=.
-FOR %%F IN (lib\*.jar) DO (set CLASSPATH=!CLASSPATH!;%%F)
-FOR %%F IN (lib\*.zip) DO (set CLASSPATH=!CLASSPATH!;%%F)
-
-java -cp %CLASSPATH% org.pentaho.md5.generator.ChecksumGenerator %*
